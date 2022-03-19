e2e02d2e27374cdd5866a268518eef71bfb0e27b
Devaprasad-sahu <97339907+Devaprasad-sahu@users.noreply.github.com>
2022-03-19 19:51:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+    region = "us-east-2"
+}
 
+
+resource "aws_instance" "sahu" {
+    ami =  "ami-03a0c45ebc70f98ea"
+    instance_type = "t2.micro"
+    key_name = "sahukey"
+
+    tags = {
+            Name = "myinstance"
+    }
+}
