fa9938043b337b04bae29a1b7ec4d70f6a771df7
sainathrudra7 <97375734+sainathrudra7@users.noreply.github.com>
2022-03-21 18:59:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
 provider "aws" {
-    region = "us-east-2"
+    region = "us-east-1"
+    aws_access_key_id = "AKIAXMZR7CJW6IJL73XF"
+    aws_secret_access_key = "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"
 }
 
 
-resource "aws_instance" "sahu" {
-    ami =  "ami-03a0c45ebc70f98ea"
+resource "aws_instance" "sainath" {
+    ami =  "ami-0e472ba40eb589f49"
     instance_type = "t2.micro"
-    key_name = "sahukey"
+    key_name = "rrr"
 
     tags = {
             Name = "myinstance"
